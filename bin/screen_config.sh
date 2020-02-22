7fe1aa4d2318af97d0527cb546123dcaedcc9d7d
Luka Krapic <luka.krapic@gmail.com>
2020-02-22 20:40:23 +0100

    update screenconfig for work laptop

--- bin/screen_config.sh
+++ bin/screen_config.sh
@@ -2,5 +2,14 @@
 
 if [[ $(xrandr | grep "HDMI-1 connected") ]]
 then
-        xrandr --auto --output HDMI-1 --mode 1920x1080 && xrandr --output eDP-1 --off
+        xrandr --auto --output HDMI-1 --mode 1920x1080 --primary --output eDP-1 --off
+        exit 0
 fi
+
+if [[ $(xrandr | grep "DP-1-3 connected") ]]
+then
+        xrandr --auto --output eDP-1 --off --output DP-1-3 --mode 1920x1080 --primary
+        exit 0
+fi
+
+exit 1
