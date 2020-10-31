9e486e0cfc21392e65dd5a5f94abe31ebbd77535
Luka Krapic <luka.krapic@gmail.com>
2020-10-31 11:27:52 +0100

    Change screen config script with new config
    Bug fix, screens are mapped to new names after the update.
    
    Change screen names, hard code them into the script. No advance logic
    just a patch.

--- bin/screen_config.sh
+++ bin/screen_config.sh
@@ -2,13 +2,13 @@
 
 if [[ $(xrandr | grep "HDMI-1 connected") ]]
 then
-        xrandr --auto --output HDMI-1 --mode 1920x1080 --primary --output eDP-1 --off
+        xrandr --auto --output HDMI-1 --mode 1920x1080 --primary --output eDP-2-3 --off
         exit 0
 fi
 
-if [[ $(xrandr | grep "DP-1-3 connected") ]]
+if [[ $(xrandr | grep "DP-2-3 connected") ]]
 then
-        xrandr --auto --output eDP-1 --off --output DP-1-3 --mode 1920x1080 --primary
+        xrandr --auto --output eDP-1 --off --output DP-2-3 --mode 1920x1080 --primary
         exit 0
 fi
 
