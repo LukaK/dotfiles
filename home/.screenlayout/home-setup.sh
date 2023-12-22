a28e9df869f99b7c6b9f61b1e0fd8de7f3c12d21
Luka Krapic <luka.krapic@gmail.com>
2023-12-22 13:10:47 +0100

    Add screen layout to home configuration files.

---
+++ home/.screenlayout/home-setup.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --off --output DP-1 --off --output DP-2 --off --output HDMI-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output DP-1-3 --off --output DP-1-4 --off --output DP-1-5 --off --output DP-1-6 --off --output DP-1-7 --off --output DP-1-8 --off
