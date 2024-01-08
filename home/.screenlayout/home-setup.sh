f95b4c4eb776e69e3c4753bf3a1304610ed80429
Luka Krapic <luka.krapic@gmail.com>
2024-01-08 08:22:15 +0100

    Update screen home setup for i3.

--- home/.screenlayout/home-setup.sh
+++ home/.screenlayout/home-setup.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
-xrandr --output eDP-1 --off --output eDP-2 --off --output DP-1 --off --output DP-2 --off --output HDMI-1-0 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output DP-1-3 --off --output DP-1-4 --off --output DP-1-5 --off --output DP-1-6 --off --output DP-1-7 --off --output DP-1-8 --off --output HDMI-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal
+xrandr \
+    --output eDP-1 --off \
+    --output eDP-2 --off \
+    --output DP-1 --off \
+    --output DP-2 --off \
+    --output DP-1-2 --off \
+    --output DP-1-3 --off \
+    --output DP-1-4 --off \
+    --output DP-1-5 --off \
+    --output DP-1-6 --off \
+    --output DP-1-7 --off \
+    --output DP-1-8 --off \
+    --output HDMI-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal \
+    --output HDMI-1-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal
